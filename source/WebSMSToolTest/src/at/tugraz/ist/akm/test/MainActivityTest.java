6f13204cd21647125cfbcc9240c92fc76776a3ec
Thomas Gödl <therock1983@gmail.com>
2012-04-08 14:42:57 +0200

    added test case template

--- source/WebSMSToolTest/src/at/tugraz/ist/akm/test/MainActivityTest.java
+++ source/WebSMSToolTest/src/at/tugraz/ist/akm/test/MainActivityTest.java
@@ -1,5 +1,32 @@
 package at.tugraz.ist.akm.test;
 
-public class MainActivityTest {
+import android.test.ActivityInstrumentationTestCase2;
+import at.tugraz.ist.akm.MainActivity;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+	private Solo solo;
+
+	public MainActivityTest() {
+		super("at.tugraz.ist.akm", MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	public void test() {
+		System.out.println("test!");
+
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
 
 }
