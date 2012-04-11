2d00a391f23ca3b1ad2f8d970f41876213713752
Stefan Lexow <stefan.lexow@gmail.com>
2012-04-11 15:32:42 +0200

    --> test commit

--- source/WebSMSTool/src/at/tugraz/ist/akm/MainActivity.java
+++ source/WebSMSTool/src/at/tugraz/ist/akm/MainActivity.java
@@ -14,5 +14,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
         Log.v("Activity", "Going to start web service");
         this.startService(new Intent(this, WebSMSToolService.class));
+        
+        System.out.println("git is fürn orsch");
+        
     }
 }
