89439df660ebc2f00ac2c5f6080afb9b2236d5ee
Raoul <rubienr@sbox.tugraz.at>
2012-05-06 00:09:29 +0200

    solves issue 13

--- source/WebSMSTool/src/at/tugraz/ist/akm/trace/Logable.java
+++ source/WebSMSTool/src/at/tugraz/ist/akm/trace/Logable.java
@@ -1,54 +1,66 @@
 package at.tugraz.ist.akm.trace;
 
-
-public class Logable {
+public class Logable
+{
 
 	private String mTag = "<notag>";
 
-	public Logable() {
+	public Logable()
+	{
 	}
 
-	public Logable(String tag) {
+	public Logable(String tag)
+	{
 		mTag = new String(tag);
 	}
 
-	public void logV(String message) {
+	public void logV(String message)
+	{
 		logV(message, null);
 	}
 
-	public void logV(String message, Throwable t) {
+	public void logV(String message, Throwable t)
+	{
 		Logger.log(Logger.LogLevel.VERBOSE, mTag, message, t);
 	}
 
-	public void logE(String message) {
+	public void logE(String message)
+	{
 		logE(message, null);
 	}
 
-	public void logE(String message, Throwable t) {
+	public void logE(String message, Throwable t)
+	{
 		Logger.log(Logger.LogLevel.ERROR, mTag, message, t);
 	}
 
-	public void logW(String message) {
+	public void logW(String message)
+	{
 		logW(message, null);
 	}
 
-	public void logW(String message, Throwable t) {
+	public void logW(String message, Throwable t)
+	{
 		Logger.log(Logger.LogLevel.WARNING, mTag, message, t);
 	}
 
-	public void logI(String message) {
+	public void logI(String message)
+	{
 		logI(message, null);
 	}
 
-	public void logI(String message, Throwable t) {
+	public void logI(String message, Throwable t)
+	{
 		Logger.log(Logger.LogLevel.INFO, mTag, message, t);
 	}
 
-	public void logD(String message) {
+	public void logD(String message)
+	{
 		logD(message, null);
 	}
 
-	public void logD(String message, Throwable t) {
+	public void logD(String message, Throwable t)
+	{
 		Logger.log(Logger.LogLevel.DEBUG, mTag, message, t);
 	}
 }
