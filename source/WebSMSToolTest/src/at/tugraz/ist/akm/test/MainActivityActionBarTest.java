fe59147a298de7870f3995b56b1a17859522af73
Stefan Lexow <stefan.lexow@gmail.com>
2012-06-01 16:08:45 +0200

    --> added a actionbar test class, but is not working. maybe we can find a solution or we delete it ;-)

---
+++ source/WebSMSToolTest/src/at/tugraz/ist/akm/test/MainActivityActionBarTest.java
@@ -0,0 +1,56 @@
+package at.tugraz.ist.akm.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import at.tugraz.ist.akm.MainActivity;
+import at.tugraz.ist.akm.test.trace.ThrowingLogSink;
+import at.tugraz.ist.akm.trace.Logable;
+import at.tugraz.ist.akm.trace.Logger;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class MainActivityActionBarTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	private Logable mLog = null;
+	
+	public MainActivityActionBarTest()
+	{
+		super("at.tugraz.ist.akm", MainActivity.class);
+		Logger.setSink(new ThrowingLogSink());
+		mLog = new Logable(MainActivityActionBarTest.class.getSimpleName());
+	}
+
+
+	public void testSettingsActivity() throws Exception {
+		Solo mSolo = new Solo(getInstrumentation(), getActivity());
+		mSolo.assertCurrentActivity("Actual activty is MainActivity", MainActivity.class);
+
+		/* no idea on how to access the action bar menu items to be clicked */
+		
+		//this should work but i am not sure if only with android 4.
+		//getInstrumentation().invokeMenuActionSync(getActivity(), R.id.info, 0);
+		
+		//Thread.sleep(3000);
+		//mSolo.assertCurrentActivity("Actual activty is AboutActivity", AboutActivity.class);
+	}
+	
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		log(getName() + ".setUp()");
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		log(getName() + ".tearDown()");
+		super.tearDown();
+	}
+
+	protected void log(final String m)
+	{
+		mLog.logV(m);
+	}
+
+}
