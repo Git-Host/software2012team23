4f9b8a01938651beee43f0c35ca99bbc05df9ce2
Stefan Lexow <stefan.lexow@gmail.com>
2012-06-01 12:09:57 +0200

    --> added back home action for actionbar

--- source/WebSMSTool/src/at/tugraz/ist/akm/AboutActivity.java
+++ source/WebSMSTool/src/at/tugraz/ist/akm/AboutActivity.java
@@ -1,6 +1,7 @@
 package at.tugraz.ist.akm;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import at.tugraz.ist.akm.actionbar.ActionBarActivity;
 
 
@@ -12,4 +13,17 @@ public class AboutActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
     }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.home:
+                finish();
+                break;
+        }
+        
+        return super.onOptionsItemSelected(item);
+    }  
 }
