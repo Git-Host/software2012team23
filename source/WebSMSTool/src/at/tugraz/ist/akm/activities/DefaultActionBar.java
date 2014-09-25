8b1e0bdbfd1090db54a05c1f86809eb20f050c6e
rubienr <rubien@gmail.com>
2014-09-25 14:30:01 +0200

    close issue69

--- source/WebSMSTool/src/at/tugraz/ist/akm/activities/DefaultActionBar.java
+++ source/WebSMSTool/src/at/tugraz/ist/akm/activities/DefaultActionBar.java
@@ -30,20 +30,21 @@ public class DefaultActionBar extends Activity
         {
         case android.R.id.home:
             Intent upIntent = NavUtils.getParentActivityIntent(this);
+
+            if (upIntent == null)
+            {
+                upIntent = new Intent(getApplicationContext(),
+                        MainActivity.class);
+            }
+
+            mLog.debug("intent [" + upIntent + "]");
             if (NavUtils.shouldUpRecreateTask(this, upIntent))
             {
-                // This activity is NOT part of this app's task, so create a new
-                // task
-                // when navigating up, with a synthesized back stack.
                 TaskStackBuilder.create(this)
-                // Add all of this activity's parents to the back stack
                         .addNextIntentWithParentStack(upIntent)
-                        // Navigate up to the closest parent
                         .startActivities();
             } else
             {
-                // This activity is part of this app's task, so simply
-                // navigate up to the logical parent activity.
                 NavUtils.navigateUpTo(this, upIntent);
             }
             return false;
