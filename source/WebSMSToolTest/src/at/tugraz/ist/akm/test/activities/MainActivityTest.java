8de17343273d3e90f0db3eb4a8f6b7e2672621ab
rubienr <rubien@gmail.com>
2014-09-29 18:54:08 +0200

    refined tests for MainActivity

--- source/WebSMSToolTest/src/at/tugraz/ist/akm/test/activities/MainActivityTest.java
+++ source/WebSMSToolTest/src/at/tugraz/ist/akm/test/activities/MainActivityTest.java
@@ -16,14 +16,19 @@
 
 package at.tugraz.ist.akm.test.activities;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import android.app.ActivityManager;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.net.wifi.WifiManager;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ToggleButton;
 import at.tugraz.ist.akm.R;
 import at.tugraz.ist.akm.activities.MainActivity;
@@ -110,6 +115,148 @@ public class MainActivityTest extends
             assertFalse(startStop.isChecked());
             stopWebService();
         }
+    }
+
+
+    public void test_backStack_homeToHome()
+    {
+        assertTrue(false);
+    }
+
+
+    private void bringMainFragmentOnTop_thenBringOhterFragmentOnTop_thenBringMainFragmentOnTop_usingBackstack_and_NavigationDrawer(
+            int otherFragmentIdxInNavigationDrawer)
+    {
+        Solo solo = new Solo(getInstrumentation(), getActivity());
+
+        dragNavigationMenu();
+
+        solo.waitForFragmentByTag(getFragmentOfNavigationDrawerMenu(0));
+        solo.clickOnView(findNavigationDrawerMenu(otherFragmentIdxInNavigationDrawer));
+        solo.waitForFragmentByTag(getFragmentOfNavigationDrawerMenu(otherFragmentIdxInNavigationDrawer));
+        assertFragmentVisible(
+                true,
+                getFragmentOfNavigationDrawerMenu(otherFragmentIdxInNavigationDrawer));
+        assertFragmentVisible(false, getFragmentOfNavigationDrawerMenu(0));
+        solo.sendKey(KeyEvent.KEYCODE_BACK);
+
+        dragNavigationMenu();
+        solo.clickOnView(findNavigationDrawerMenu(0));
+        solo.waitForFragmentByTag(getFragmentOfNavigationDrawerMenu(0));
+        assertFragmentVisible(true, getFragmentOfNavigationDrawerMenu(0));
+        assertFragmentVisible(
+                false,
+                getFragmentOfNavigationDrawerMenu(otherFragmentIdxInNavigationDrawer));
+    }
+
+
+    private void assertFragmentVisible(boolean visible, String viewTag)
+    {
+        Solo solo = new Solo(getInstrumentation(), getActivity());
+        Fragment view = solo.getCurrentActivity().getFragmentManager()
+                .findFragmentByTag(viewTag);
+
+        if (visible)
+        {
+            assertTrue(view != null);
+
+        } else
+        {
+            if (view != null)
+            {
+                assertFalse(view.isVisible());
+            } else
+            {
+                assertTrue(view == null);
+            }
+        }
+    }
+
+
+    private void dragNavigationMenu()
+    {
+        Solo solo = new Solo(getInstrumentation(), getActivity());
+        Point size = new Point();
+        getActivity().getWindowManager().getDefaultDisplay().getSize(size);
+
+        solo.drag(0, size.y / 2, size.x / 2, size.x / 2, 10);
+    }
+
+
+    private String getFragmentOfNavigationDrawerMenu(int idx)
+    {
+        return getActivity().getResources().getStringArray(
+                R.array.drawer_fragment_array)[idx];
+    }
+
+
+    private String getNameOfNavigationDrawerMenu(int idx)
+    {
+        return getActivity().getResources().getStringArray(
+                R.array.drawer_string_array)[idx];
+    }
+
+
+    private View findNavigationDrawerMenu(int id)
+    {
+        Solo solo = new Solo(getInstrumentation(), getActivity());
+        try
+        {
+            View menuEntry = solo.getText(getNameOfNavigationDrawerMenu(id));
+            return menuEntry;
+        }
+        catch (Throwable e)
+        {
+            return null;
+        }
+    }
+
+
+    private void logViews(ArrayList<View> views)
+    {
+        int numIdLessViews = 0;
+        int numViews = 0;
+
+        for (View v : views)
+        {
+            String name = "-";
+            numViews++;
+            try
+            {
+                name = getActivity().getResources().getResourceName(v.getId());
+                mLog.debug("*[" + name + "][" + v.getId() + "] ");
+            }
+            catch (Throwable e)
+            {
+                numIdLessViews++;
+            }
+        }
+        mLog.debug("found [" + numViews + "] and suppressed [" + numIdLessViews
+                + "] views");
+    }
+
+
+    public void test_backStack_messagesToHome()
+    {
+        bringMainFragmentOnTop_thenBringOhterFragmentOnTop_thenBringMainFragmentOnTop_usingBackstack_and_NavigationDrawer(1);
+    }
+
+
+    public void test_backStack_settingsToHome()
+    {
+        bringMainFragmentOnTop_thenBringOhterFragmentOnTop_thenBringMainFragmentOnTop_usingBackstack_and_NavigationDrawer(2);
+    }
+
+
+    public void test_backStack_aboutToHome()
+    {
+        bringMainFragmentOnTop_thenBringOhterFragmentOnTop_thenBringMainFragmentOnTop_usingBackstack_and_NavigationDrawer(3);
+    }
+
+
+    public void test_backStack_multipletimesSettingsKlickedToHome()
+    {
+        assertTrue(false);
     }
 
 
